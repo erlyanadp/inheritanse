a9be880c08b991c1782371bf8b4799d421581022
SMK TELKOM <SMK TELKOM@192.10.17.2>
2017-09-08 01:10:23 +0700

    modul 3 inheritanse

---
+++ src/praktikum/Turunan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum;
+
+/**
+ *
+ * @author SMK TELKOM
+ */
+public class Turunan extends Induk {
+    public void test(String s){
+        System.out.println("Method di dalam kelas Turunan");
+        System.out.println("s : \"" + s + "\"");
+    }
+}
