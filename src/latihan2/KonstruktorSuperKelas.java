62ba33f51cf55df6e85d086d4cdc44d2768c7617
SMK TELKOM <SMK TELKOM@192.10.17.2>
2017-09-08 00:43:47 +0700

    modul 3 inheritanse

---
+++ src/latihan2/KonstruktorSuperKelas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author SMK TELKOM
+ */
+public class KonstruktorSuperKelas 
+{
+    public static void main (String [] args)
+    {
+        employ programer1 = new employ ("12345678", "Yanto", 32);
+        programer1.info();
+        
+    }
+}
